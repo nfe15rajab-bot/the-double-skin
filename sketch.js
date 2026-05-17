47403ba08ef531e2e916858f6b13becd8d8498a0
nfe15rajab-hub <nfe15rajab@gmail.com>
2026-05-17 07:27:30 +0100

    star pattern

--- sketch.js
+++ sketch.js
@@ -1,14 +1,82 @@
 const canvas = document.getElementById('canvas')
 const pen = canvas.getContext('2d')
+let mode = 'hex'
+
 
 let mouseX = 0
 let mouseY = 0
+
+canvas.width = window.innerWidth
+canvas.height= window.innerHeight
+
+window.addEventListener('resize', () => {
+    canvas.width = window.innerWidth
+    canvas.height = window.innerHeight
+})
+document.getElementById('toggle').addEventListener('click', function(){
+    mode = mode ==='hex' ? 'star' : 'hex'
+    this.textContent = mode === 'hex' ? 'Mode: Hex' : 'Mode: Star'
+})
+// const radius = 30
+// const col = 20
+// const rows = 15
+// // canvas.width = cols * radius * Math.sqrt(3)
+// // canvas.height = rows * radius * 1.5
+
+
+
+// let mouseX = 0
+// let mouseY = 0
 
 canvas.addEventListener('mousemove', function(e){
     const rect = canvas.getBoundingClientRect()
     mouseX=e.clientX - rect.left
     mouseY=e.clientY - rect.top }
 )
+function drawStarHex(cx, cy, radius, d){
+    const corners=[]
+    for (let i = 0; i<6; i++){
+        const angle = (Math.PI /180) * (60 * i - 30)
+        corners.push({
+            x: cx + radius * Math.cos(angle),
+            y: cy + radius * Math.sin(angle)
+        })
+    }
+    const mids = []
+    for (let i=0; i<6; i++){
+        const a =corners[i]
+        const b =corners[(i+1)%6]
+
+        const mx = (a.x + b.x)/2
+        const my = (a.y + b.y)/2
+        
+        const dx = cx - mx
+        const dy = cy - my
+
+        const dist = Math.sqrt(dx * dx + dy * dy)
+        mids.push({
+            x: mx + (dx/dist)*d,
+            y: my + (dy/dist)*d
+        })
+    }
+
+    const points =[]
+    for (let i=0; i<6; i++){
+        points.push(corners[i])
+        points.push(mids[i])
+    }
+    pen.beginPath()
+    pen.moveTo(points[0].x, points[0].y)
+    for (let i=1 ; i<points.length; i++){
+        pen.lineTo(points[i].x, points[i].y)
+    }
+    pen.strokeStyle = 'rgb(0, 0, 0)'
+    pen.lineWidth = 5
+    pen.fillStyle =  'rgb(79, 79, 91)'
+    pen.closePath()
+    pen.stroke()
+    pen.fill()
+    }
 
 function drawHexagon(cx, cy, radius){
     const dx = cx - mouseX
@@ -16,25 +84,39 @@ function drawHexagon(cx, cy, radius){
     const dist = Math.sqrt(dx * dx  + dy * dy)
     const sigma = 600
     const falloff = Math.exp(-(dist * dist)/(2* sigma * sigma))
+
+    if (mode==='star'){
+        const d = Math.min(radius, Math.max(radius*0.1, falloff * radius))
+        pen.beginPath()
+        drawStarHex(cx, cy, radius, d)
+    } else {
+
+
     const scale = Math.min(1, Math.max(0.1, 1-falloff * 0.9))
     const scaledRadius = radius * scale
-    pen.lineWidth = 0.5
-
+    pen.strokeStyle = 'rgb(255, 255, 255)'
+    pen.lineWidth = 10
     pen.beginPath()
-            for(let i=0; i<6 ; i++){
-                const angle = (Math.PI / 180) * (60 * i - 30)
-                const x = cx + scaledRadius * Math.cos(angle)
-                const y = cy + scaledRadius * Math.sin(angle)
-                if (i === 0 ) pen.moveTo(x,y)
-                else pen.lineTo(x, y)
+    //pen.fill()
+    pen.fillStyle = 'hsl(244, 33%, 42%)'
+
+    
+    for(let i=0; i<6 ; i++){
+        const angle = (Math.PI / 180) * (60 * i - 30)
+        const x = cx + scaledRadius * Math.cos(angle)
+        const y = cy + scaledRadius * Math.sin(angle)
+        if (i === 0 ) pen.moveTo(x,y)
+        else pen.lineTo(x, y)
     }
 
 
     pen.closePath()
     pen.stroke()
-    const alpha = (1 - scale) * 0.95
-    pen.fillStyle = `rgba(230, 211, 42, ${alpha})`
+    //const alpha = (1 - scale) * 0.95
+    //pen.fillStyle = `rgba(230, 211, 42, ${alpha})`
     pen.fill()
+    //drawStar(cx, cy, scaledRadius*0.9, 0.65)
+    }
 }
 function drawGrid(cols, rows, radius){
     const spacingX = radius * Math.sqrt(3)
@@ -45,16 +127,31 @@ function drawGrid(cols, rows, radius){
             const offset = (row % 2)*(spacingX/2)
             const x =col * spacingX+offset
             const y =row * spacingY
-            drawHexagon(x, y, radius)
+            drawHexagon(x, y, radius, mode)
         }
     }
 }
     
 
+    
 function animate() {
     pen.clearRect(0, 0, canvas.width, canvas.height)
-    const sizeGrid=70
-    drawGrid(sizeGrid, sizeGrid, sizeGrid)
+    pen.fillStyle = 'rgb(147, 204, 254)'
+    pen.fillRect(0, 0, canvas.width, canvas.height)
+
+    //necessary to cut the hexagonal part
+    pen.globalCompositeOperation = 'destination-out'
+    //const sizeGrid=50
+    const radius = 200
+    const cols = Math.ceil(canvas.width / (radius * Math.sqrt(3))) + 1
+    const rows = Math.ceil(canvas.height / (radius * 1.5)) + 1
+
+    drawGrid(cols, rows, radius)
+
+    pen.globalCompositeOperation = 'source-over'
+    //pen.fillStyle='rgb(239, 182, 182)'
+    pen.strokeStyle='rgb(25, 5, 147)'
+    drawGrid(cols, rows, radius)
     requestAnimationFrame(animate)
 }
 animate()
